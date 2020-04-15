3c2b96db2b8e9569891f330284106cd4df4e67a7
jamalka85b15 <crutchfield.nur@gmail.com>
2020-04-14 22:12:39 -0500

    first

---
+++ src/test/java/com/vytrack/pages/DashboardPage.java
@@ -0,0 +1,11 @@
+package com.vytrack.pages;
+
+public  class DashboardPage extends AbstractPageBase {
+
+    // public DashboardPage(){
+    // PageFactory.initElements(Driver.getDriver(),this);
+    //}
+
+
+
+}
