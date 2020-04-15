49bff2c7c8a8273e57510f64524af2312639bfda
jamalka85b15 <crutchfield.nur@gmail.com>
2020-04-14 23:26:58 -0500

    Steps

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinition.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinition.java
@@ -1,4 +1,23 @@
 package com.vytrack.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginStepDefinition {
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+
+    }
+
+    @When("user logs as a sales manager")
+    public void user_logs_as_a_sales_manager() {
+
+    }
+
+    @Then("user should verify that title is Dashboard")
+    public void user_should_verify_that_title_is_Dashboard() {
+
+    }
+
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -3,4 +3,7 @@ Feature: Login
 
   # Test Method=Test Case= Scenario
   # Test + Data Provider=Scenario Outline +Example table
-  Scenario:
+  Scenario:Login as  manager and verify that title is Dashboard
+    Given user is on login page
+    When user logs as a sales manager
+    Then user should verify that title is Dashboard
